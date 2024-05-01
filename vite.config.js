fe57331fd2ee5c94c58bf34f893016494297aa8f
djonali <flexjonali@gmail.com>
2024-05-01 16:02:27 +0530

    updating vit config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir: "build"
+  }
 })
+
+
+
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+
+// export default defineConfig({
+//   plugins: [reactRefresh()],
+// });
+
